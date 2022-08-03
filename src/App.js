9301a9f34cb5cfe4d584672b41da31bfec9541e7
callytat <104803387+callytat@users.noreply.github.com>
2022-08-02 20:06:42 -0700

    Added dictionary component and search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <main>
+          <Dictionary />
+        </main>
+        <footer>Coded by Cally Tat</footer>
+      </div>
     </div>
   );
 }
